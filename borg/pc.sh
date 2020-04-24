54dc72667d5887f8319ffcf5d39a942a387544ca
Jake <git@ja-ke.tech>
2020-04-24 17:07:22 +0200

    Use pattern instead of tags for PC

--- borg/pc.sh
+++ borg/pc.sh
@@ -7,4 +7,4 @@ then
 fi
 pwd
 
-borg create -svp -C lz4 "PC::Home-{now:%y.%j}" /home/jk/ --keep-exclude-tags --exclude-caches --exclude-if-present PUB_DL.TAG --exclude-if-present VM.TAG "$@"
+borg create -svp -C lz4 --patterns-from pattern.lst "PC::Home-{now:%y.%j}" /home/jk/ "$@"
