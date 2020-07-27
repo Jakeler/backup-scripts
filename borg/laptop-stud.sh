bdf25bbbf8ed726915100df446d29488e77f978f
Jake <git@ja-ke.tech>
2020-07-27 23:51:55 +0200

    Account for synced folders

--- borg/laptop-stud.sh
+++ borg/laptop-stud.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source chg_dir.sh
 
-borg create -svp -C lz4 --patterns-from pattern.lst "Laptop::Home_t440s-{now:%y.%j}" ~/
+borg create -svp -C lz4 --patterns-from pattern.lst --pattern '! /home/*/Hobby/' "Laptop::Home_t440s-{now:%y.%j}" ~/
