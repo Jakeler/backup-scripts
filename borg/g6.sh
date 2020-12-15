aff7b848b0a6c5d275d6fc2d6849a55d151f0b4f
Jake <git@ja-ke.tech>
2020-12-15 21:38:32 +0100

    More excludes

--- borg/g6.sh
+++ borg/g6.sh
@@ -6,8 +6,10 @@ source chg_dir.sh
 mkdir -p $MNT
 simple-mtpfs $MNT
 
+[ $# -eq 1 ] && exclude_apk="--pattern '- **/TitaniumBackup/*.apk'"
+
 if [ "$(ls -A $MNT)" ]; then
-    borg create -svp -C lz4 "Phone::TitaniumBackup-{now:%y.%j}" "$MNT/Interner gemeinsamer Speicher/TitaniumBackup" "$MNT/disk/TitaniumBackup/"
+    borg create -svp -C lz4 $exclude_apk "Phone::TitaniumBackup-{now:%y.%j}" "$MNT/Interner gemeinsamer Speicher/TitaniumBackup" "$MNT/disk/TitaniumBackup/"
     borg create -svp -C lz4 "Multimedia::G6.DCIM-{now:%y.%j}" "$MNT/Interner gemeinsamer Speicher/DCIM/" "$MNT/disk/DCIM/"
     #borg create -svp -C lz4 "Phone::TWRP-{now:%y.%j}" $MNT/TWRP/
 
