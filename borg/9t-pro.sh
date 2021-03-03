0912f16f03e46ff6b1385d825a14e3cdc5f92fa6
Jake <git@ja-ke.tech>
2021-03-04 00:52:13 +0100

    Add 9t pro script with OABX

---
+++ borg/9t-pro.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+MNT=/tmp/MOUNT
+
+source chg_dir.sh
+
+### simple-mtpfs does not work anymore, uses go-mtpfs from Google dev instead
+mkdir -p $MNT
+go-mtpfs $MNT & sleep 3
+
+[ $# -eq 1 ] && exclude_apk="--pattern '- **/*.apk'"
+
+if [ "$(ls -A $MNT)" ]; then
+    borg create -svp -C lz4 $exclude_apk "Phone::OABX+Signal-{now:%y.%j}" "$MNT/Interner gemeinsamer Speicher/OABX" "$MNT/Interner gemeinsamer Speicher/Signal"
+    borg create -svp -C lz4 --pattern '- **/*.mp4' "Multimedia::9T.DCIM-{now:%y.%j}" "$MNT/Interner gemeinsamer Speicher/DCIM/"
+    #borg create -svp -C lz4 "Phone::TWRP-{now:%y.%j}" $MNT/TWRP/
+
+    fusermount -u $MNT
+else
+    echo "MTP mount did not work!"
+    exit 1
+fi
