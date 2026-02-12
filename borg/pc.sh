a3303a28fb44a96ed10956feca868f88c2c0f100
Jake <git@jklr.org>
2026-02-12 04:29:20 +0100

    Update borg patterns

--- borg/pc.sh
+++ borg/pc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source chg_dir.sh
 
-borg create -svp -C lz4 --patterns-from pattern.lst --pattern '! /home/*/Doks/' "PC::Home-{now:%y.%j}" /home/jk/ "$@"
+borg create -svp -C lz4 --patterns-from pattern.lst --pattern '! /home/*/Doks/' "PC::Home-{now:%y.%j}" /home/jk/
