0ef2ec62a691b503c434019db26689a6c9432cfd
Jake <git@jklr.org>
2026-02-25 07:00:29 +0100

    Update pc scripts and patterns

--- borg/laptop.sh
+++ borg/laptop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source chg_dir.sh
+# source chg_dir.sh # use BORG_BASEDIR with ssh
 
-borg create -svp -C zstd --patterns-from pattern.lst --pattern '! /home/*/Hobby/' "Laptop::Home_zenbook13-{now:%y.%j}" ~/
+borg create -svp -C zstd --patterns-from pattern.lst --pattern '! /home/*/Hobby/' --pattern '! /home/*/DBs/' "$BORG_BASEDIR/Laptop::Home_zenbook13-{now:%y.%j}" ~/
--- borg/pc.sh
+++ borg/pc.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-source chg_dir.sh
+# source chg_dir.sh # use BORG_BASEDIR with ssh
 
-borg create -svp -C zstd --patterns-from pattern.lst --pattern '! /home/*/Doks/' "PC::Home-{now:%y.%j}" /home/jk/
+# recommeded prep:
+# delete browser cache (chrome, firefox) and close
+# clear cache in Thunderbird settings, right click on account - compact all folders
+# clear dolphin trash can
+
+borg create -svp -C zstd --patterns-from pattern.lst --pattern '! /home/*/Doks/' "$BORG_BASEDIR/PC::Home-{now:%y.%j}" /home/jk/
+# might exlude DBs here, but it's small and important, maybe good to have 2x
